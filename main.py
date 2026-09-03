72d6bcae3549e1a67f3e149d8e67cb6ea3a6199a
Zeno Maccari <zeno.maccari@studio.unibo.it>
2026-09-03 15:55:27 +0200

    fix: update URL handling with base_url

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 
-main_page_url = "https://tirocini.unibo.it/tirocini/studenti/gestioneaziendeconautocandidature.htm?page="
+LISTING_PATH = "gestioneaziendeconautocandidature.htm"
+BASE__URL = "https://tirocini.unibo.it/tirocini/studenti/"
 
 
 def setup_payload():
@@ -26,8 +27,12 @@ def setup_cookies():
     return {"JSESSIONID": os.getenv('JSESSIONID')}
 
 
-def send_post_req(url, cookies, page_num, payload):
-    return requests.post(url+str(page_num), headers="", cookies=cookies, data=payload)
+def send_post_req(url, cookies, payload, page_num=1):
+    return requests.post(url+"?page="+str(page_num), cookies=cookies, data=payload)
+
+
+def send_get_req(url, cookies):
+    return requests.get(url=url, cookies=cookies)
 
 
 def parse_links(page):
@@ -36,17 +41,22 @@ def parse_links(page):
     table = soup.find('table', class_="iceDataTblOutline")
     rows += table.find_all('tr', class_="rigaPari")
     rows += table.find_all('tr', class_="rigaDispari")
-    return [row.td.a["href"] for row in rows]
+    return [BASE__URL+row.td.a["href"] for row in rows]
+
+
+def parse_company_page(page):
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup.find("table", class_="tbSimpleData")
 
 
 def main():
     payload = setup_payload()
     cookies = setup_cookies()
-    p = send_post_req(main_page_url, cookies=cookies,
+    p = send_post_req(BASE__URL+LISTING_PATH, cookies=cookies,
                       payload=payload, page_num=1)
     links = parse_links(p)
-    for item in links:
-        print(item)
+    page = send_get_req(links[0], cookies=cookies)
+    print(parse_company_page(page))
 
 
 if __name__ == "__main__":
