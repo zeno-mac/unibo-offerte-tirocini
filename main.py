9c0cdbe87021a41e81657cdf367007c2fad5684e
Zeno Maccari <zeno.maccari@studio.unibo.it>
2026-09-03 20:23:29 +0200

    chore: refactor file paths for CSV and JSON outputs; improve code formatting and readability

--- main.py
+++ main.py
@@ -75,26 +75,30 @@ def fetch_all_listing_pages(url, cookies, payload, max_pages=7, max_workers=3):
 
     return results
 
+
 def write_csv(file):
     fieldnames = list(dict.fromkeys(key for info in file for key in info))
-    with open("log.csv", "w", newline="", encoding="utf-8") as f:
+    with open("files/log.csv", "w", newline="", encoding="utf-8") as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames, restval="")
         writer.writeheader()
         writer.writerows(file)
-       
+
+
 def write_json(file):
-    with open("log.json", "w") as f:
-        json.dump(file, f, ensure_ascii=False, indent=2) 
-        
+    with open("files/log.json", "w") as f:
+        json.dump(file, f, ensure_ascii=False, indent=2)
+
 
 def format_number(number):
     return "0"*(3 - len(str(number))) + str(number)
+
+
 def main():
     payload = setup_payload()
     cookies = setup_cookies()
     print("Fetching listing pages...")
     pages = fetch_all_listing_pages(url=BASE__URL+LISTING_PATH,
-                     cookies=cookies, payload=payload)
+                                    cookies=cookies, payload=payload)
     companies = []
     print("Extracting companies urls...")
     for page in pages:
@@ -104,19 +108,20 @@ def main():
     print("Fetching and extracting companies pages...")
     counter = 1
     for company in companies:
-        if(counter == 1):
-            sys.stdout.write(f"Azienda {format_number(counter)}/{len(companies)}")
+        if (counter == 1):
+            sys.stdout.write(
+                f"Azienda {format_number(counter)}/{len(companies)}")
         else:
             sys.stdout.write("\b"*7)
             sys.stdout.write(f"{format_number(counter)}/{len(companies)}")
         sys.stdout.flush()
-        counter+=1
+        counter += 1
         page = fetch_company_page(url=company["url"], cookies=cookies)
-        companies_info.append(extract_company_info(page.content, company["url"]))
-        
+        companies_info.append(extract_company_info(
+            page.content, company["url"]))
+
     write_csv(companies_info)
     write_json(companies_info)
-
 
 
 if __name__ == "__main__":
