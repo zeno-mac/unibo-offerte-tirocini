c2ddfc836d418791106db82ef83f6829cb68762a
Zeno Maccari <zeno.maccari@studio.unibo.it>
2026-09-03 15:27:47 +0200

    refactor: move parsing logic to separate function

--- main.py
+++ main.py
@@ -24,29 +24,27 @@ def setup_cookies():
     return {"JSESSIONID" : os.getenv('JSESSIONID')}
                 
         
-def send_post_req(url, cookies, index, payload):
-    return requests.post(url+str(index),headers="",cookies=cookies,data=payload)
+def send_post_req(url, cookies, page_num, payload):
+    return requests.post(url+str(page_num),headers="",cookies=cookies,data=payload)
+
+def parse_links(page):
+    rows=[]
+    soup = BeautifulSoup(page.content, "html.parser")
+    table =  soup.find('table', class_ = "iceDataTblOutline")
+    rows += table.find_all('tr', class_="rigaPari")
+    
+    rows += table.find_all('tr', class_="rigaDispari")
+    return [row.td.a["href"] for row in rows]
 
 
 def main():
     payload = setup_payload()
     cookies = setup_cookies()
-    rows = []
-    for i in range(1,8):
-        res = send_post_req(url=main_page_url,cookies= cookies, payload=payload, index=i)
-        soup = BeautifulSoup(res.content, "html.parser")
-        table =  soup.find('table', class_ = "iceDataTblOutline")
-        rows += table.find_all('tr', class_="rigaPari")
-        rows += table.find_all('tr', class_="rigaDispari")
     
-
-    list = []
-    for row in rows:
-        list.append((row.td.a["href"]))
-    for item in list:
+    p = send_post_req(main_page_url,cookies=cookies,payload=payload, page_num=1)
+    links = parse_links(p)
+    for item in links:
         print(item)
-    print(f"Numero oggetti: {len(list)}")
-    
     
 if __name__ == "__main__":
     main()
